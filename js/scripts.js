0814162faca723eb2e74279c867597a1ff6d2be1
Chris Quattrocchi and Lena Ha <lena@>
2017-11-02 09:48:48 -0700

    set variables for player names, implement die-roller

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,22 @@
-function Player(name, tempScore, totalScore){
+function Player(name, tempScore, totalScore) {
   this.name = name
   this.tempScore = tempScore
   this.totalScore = totalScore
 }
+
+$("#roll-btn").click(function() {
+  var roll = Math.floor(Math.random() * 6) + 1;
+});
+
+$(document).ready(function() {
+  $("#player-names").submit(function(event) {
+    event.preventDefault();
+    var p1name = $("input#p1").val();
+    var p2name = $("input#p2").val();
+    var player1 = new Player(p1name, 0, 0);
+    var player2 = new Player(p2name, 0, 0);
+    $(".pre-game").hide();
+    $(".game-start").show();
+
+  }); //when player-names us submitted
+}); //end doc ready
